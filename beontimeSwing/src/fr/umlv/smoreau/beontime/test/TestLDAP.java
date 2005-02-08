79274c9ce853b88143fb1c33e793cbc0bb65a4be
cstefkivoila <cstefkivoila>
2005-02-08 19:34:25 +0000

    envoi de la bonne version du test LDAP

--- beontimeSwing/src/fr/umlv/smoreau/beontime/test/TestLDAP.java
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/test/TestLDAP.java
@@ -1,82 +1,56 @@
 package fr.umlv.smoreau.beontime.test;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.*;
+import javax.naming.*;
+import javax.naming.directory.*;
 
-import javax.naming.NamingException;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.InitialDirContext;
-
-
-/* DESS CRI - projet BeOnTime */
-/* Created on 12 janv. 2005 */
-
-/**
- * @author abrunete
- */
 public class TestLDAP {
 
-	private String host  = "ldapetud.univ-mlv.fr";
+    public static String CONTEXT = "com.sun.jndi.ldap.LdapCtxFactory";
+    public static String HOST = "ldap://ldapetud.univ-mlv.fr:389";
+    public static String BASE = "ou=Etudiant,dc=univ-mlv,dc=fr";
 
-	private Connection conn;
+    public static void main(String [] args) {
 
-	public TestLDAP() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	protected void connect() {
-		
-		try{
-			Class.forName("com.octetstring.jdbcLdap.sql.JdbcLdapDriver").newInstance();
-			conn = java.sql.DriverManager.getConnection(
-					"jdbc:ldap://"+host+"/ou=Etudiant,dc=univ-mlv,dc=fr");
-		} catch(Exception e){
-			System.out.println("Connection ratée: "+e);
-		//	e.printStackTrace();
-			System.exit(-1);  
-		}
-		
-	}
-	
-	protected ResultSet request() {
-		ResultSet res = null;
-		
-		
-		String req = "SELECT * FROM ou=Users WHERE uid=abrunete";
-		
-		try {
-			Statement stmt = conn.createStatement();
-			res = stmt.executeQuery(req);
-		} catch (SQLException e) {
-			System.err.println("Erreur de requête");
-			e.printStackTrace();
-		}
-		return res;
-				
-		
-	}
-	
-	public void essai() {
-		try {
-			DirContext ctx = new InitialDirContext();
-			//String[] attrIDs = ()
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		TestLDAP ldap = new TestLDAP();
-		
-		ldap.connect();
-		System.out.println("--- connection OK");
-		
-		ResultSet res = ldap.request();
-		System.out.println(res);
-		
-	}
+        String filter = "(uid=forax)";
+
+        try {
+            Hashtable env = new Hashtable();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, CONTEXT);
+            env.put(Context.PROVIDER_URL, HOST);
+           
+            DirContext ctx = new InitialDirContext(env);
+
+            SearchControls sc = new SearchControls();
+            sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            
+            NamingEnumeration items = ctx.search(BASE, filter, sc);
+
+            while (items != null && items.hasMore()) {
+
+                SearchResult sr = (SearchResult)items.next();
+                String dn = sr.getName();
+                System.out.println("DN : " + dn);
+                
+                Attributes attrs = sr.getAttributes();
+                NamingEnumeration attributs = attrs.getAll();
+
+                while (attributs != null && attributs.hasMore()) {
+                    
+                    Attribute attr = (Attribute)attributs.next();
+                    String attrId = attr.getID();
+                    System.out.println("Attribut : " + attrId + " : ");
+                    Enumeration values = attr.getAll();
+
+                    while (values != null && values.hasMoreElements()) {
+                        
+                        System.out.println("\t" + values.nextElement() + "\n");
+                    }
+                }
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
