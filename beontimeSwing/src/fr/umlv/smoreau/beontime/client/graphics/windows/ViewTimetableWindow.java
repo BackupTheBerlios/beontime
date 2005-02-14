525ffa58b410a151344b377ea6c90be6058a994d
smalouin2004 <smalouin2004>
2005-02-14 13:33:56 +0000

    *** empty log message ***

--- beontimeSwing/src/fr/umlv/smoreau/beontime/client/graphics/windows/ViewTimetableWindow.java
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/client/graphics/windows/ViewTimetableWindow.java
@@ -3,16 +3,44 @@
  */
 package fr.umlv.smoreau.beontime.client.graphics.windows;
 
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
 /**
  * @author BeOnTime
  */
 public class ViewTimetableWindow {
 
+	
+	private JLabel visuEDTLabel;
+	private JLabel choixEDTLabel;
+	private JComboBox jcbTypeEDT;
+	private JComboBox jcbSubjectEDT;
+	
     /* (non-Javadoc)
      * @see fr.umlv.smoreau.beontimeSwing.graphics.windows.Window#show(java.lang.Object[])
      */
     public void show() {
         // TODO Raccord de méthode auto-généré
+    }
+    
+    
+    
+    public void addComponent(GridBagLayout gbLayout,GridBagConstraints constraints,Component comp,int gridwidth, int gridheight, double weightx, double weighty, int anchor, int fill, Insets insets) {
+        constraints. gridwidth= gridwidth;
+        constraints. gridheight = gridheight;
+        constraints.weightx = weightx;
+        constraints.weighty = weighty;
+        constraints.anchor = anchor;
+        constraints.fill = fill;
+        constraints.insets = insets;
+ 
+        gbLayout.setConstraints(comp,constraints);
     }
 
 }
