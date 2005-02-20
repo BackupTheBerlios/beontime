20214b3e4c90cf9335da9843b867f4a7097ad20f
cstefkivoila <cstefkivoila>
2005-02-20 22:24:32 +0000

    petite modif sur les timetable

--- beontime/src/fr/umlv/smoreau/beontime/filter/TimetableFilter.java
+++ beontime/src/fr/umlv/smoreau/beontime/filter/TimetableFilter.java
@@ -1,19 +1,43 @@
-/*
- * 
- */
 package fr.umlv.smoreau.beontime.filter;
+
+import java.util.HashMap;
+
+import fr.umlv.smoreau.beontime.model.timetable.Timetable;
 
 /**
  * @author BeOnTime
  */
-public class TimetableFilter implements Filter {
+public class TimetableFilter extends Timetable implements Filter {
+    private static final HashMap corres;
+    
+    static {
+        corres = new HashMap();
+        corres.put("Group.IdGroupe", "id_groupe");
+        corres.put("Formation.IdFormation", "id_formation");
+    }
+
+
+    public TimetableFilter() {
+        super();
+    }
+    
+    public TimetableFilter(Timetable timetable) {
+        super();
+        try {
+            FilterUtils.fillFilterClass(this, timetable, corres.keySet());
+        } catch (Exception e) {
+            throw new RuntimeException("Erreur lors de l'introspection", e);
+        }
+    }
 
 	/* (non-Javadoc)
 	 * @see fr.umlv.smoreau.beontime.filter.Filter#getQueryHQL()
 	 */
 	public String getHQLQuery() {
-		// TODO Auto-generated method stub
-		return null;
+	    try {
+            return FilterUtils.createHQLQuery(this, corres);
+        } catch (Exception e) {
+            throw new RuntimeException("Erreur lors de l'introspection", e);
+        }
 	}
-
 }
