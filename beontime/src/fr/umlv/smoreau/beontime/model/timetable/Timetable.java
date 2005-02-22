2dbbe634c18a01d0ba50fa191cc9fa0fc7a36f5d
cstefkivoila <cstefkivoila>
2005-02-22 00:01:09 +0000

    suppression d'un champ inutil

--- beontime/src/fr/umlv/smoreau/beontime/model/timetable/Timetable.java
+++ beontime/src/fr/umlv/smoreau/beontime/model/timetable/Timetable.java
@@ -1,9 +1,5 @@
-/*
- * 
- */
 package fr.umlv.smoreau.beontime.model.timetable;
 
-import fr.umlv.smoreau.beontime.model.*;
 import fr.umlv.smoreau.beontime.model.Group;
 
 /**
@@ -11,10 +7,9 @@ import fr.umlv.smoreau.beontime.model.Group;
  */
 public class Timetable {
     private Group group;
-    private Formation formation;
     private Subject[] fields;
     private Course[] courses;
-    
+
     public Timetable() {
     }
 
@@ -24,42 +19,35 @@ public class Timetable {
     public Course[] getCourses() {
         return courses;
     }
+
     /**
      * @param courses courses à définir.
      */
     public void setCourses(Course[] courses) {
         this.courses = courses;
     }
+
     /**
      * @return Renvoie fields.
      */
     public Subject[] getFields() {
         return fields;
     }
+
     /**
      * @param fields fields à définir.
      */
     public void setFields(Subject[] fields) {
         this.fields = fields;
     }
-    /**
-     * @return Renvoie formation.
-     */
-    public Formation getFormation() {
-        return formation;
-    }
-    /**
-     * @param formation formation à définir.
-     */
-    public void setFormation(Formation formation) {
-        this.formation = formation;
-    }
+
     /**
      * @return Renvoie group.
      */
     public Group getGroup() {
         return group;
     }
+
     /**
      * @param group group à définir.
      */
