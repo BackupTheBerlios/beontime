a330f97512e670a56ecb14b7c3ccc44fb3b84225
smalouin2004 <smalouin2004>
2005-02-24 17:00:52 +0000

    Listener pour le dynamisme des boutons plus des formulaires

---
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/forms/ButtonPlusListener.java
@@ -0,0 +1,66 @@
+package fr.umlv.smoreau.beontime.client.actions.forms;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * @author BeOnTime
+ */
+public class ButtonPlusListener implements ActionListener {
+
+	private JPanel ownPanel;
+	private JPanel jcbPanel;
+	
+	private JFrame formFrame;
+	
+	public ButtonPlusListener(JPanel ownPanel, JPanel jcbPanel, JFrame formFrame) {
+		this.ownPanel = ownPanel;
+		this.jcbPanel = jcbPanel;
+		
+		this.formFrame = formFrame;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+		
+		String text = ((JButton)e.getSource()).getText();
+		
+		if(text.compareTo("+") == 0) {
+			
+			jcbPanel.add(new JComboBox());
+			jcbPanel.add(Box.createVerticalStrut(5));
+		
+			JButton plus = new JButton("+");
+			plus.addActionListener(new ButtonPlusListener(ownPanel, jcbPanel, formFrame));
+			ownPanel.add(plus);
+			ownPanel.add(Box.createVerticalStrut(5));
+		
+			((JButton)e.getSource()).setText("x");
+			
+			formFrame.pack();
+		}
+		
+		else {
+			//int pos = ((JButton)e.getSource()).getComponentCount();
+			int position = ownPanel.getComponentZOrder(((JButton)e.getSource()));
+			
+			jcbPanel.remove(position);
+			jcbPanel.remove(position);
+			
+			ownPanel.remove(position);
+			ownPanel.remove(position);
+			
+			formFrame.pack();
+			//System.out.println(""+position);
+		}
+	}
+
+}
