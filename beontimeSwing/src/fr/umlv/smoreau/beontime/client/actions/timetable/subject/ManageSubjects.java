721c2f232d3a5d66aad5b32a48bf21f33289008e
smalouin2004 <smalouin2004>
2005-03-03 00:05:53 +0000

    mise a jour de ManageSubject

--- beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/timetable/subject/ManageSubjects.java
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/timetable/subject/ManageSubjects.java
@@ -34,7 +34,7 @@ public class ManageSubjects extends Action {
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
      */
     public void actionPerformed(ActionEvent arg0) {
-        ManageElementsWindow window = new ManageElementsWindow();
+        ManageElementsWindow window = new ManageElementsWindow("TYPE_SUBJECTS");
         window.show();
     }
 }
