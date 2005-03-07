83e535d2e69d904c5ab0381f421f6f931f078bda
cstefkivoila <cstefkivoila>
2005-03-07 15:22:04 +0000

    implémentation de l'action impression

--- beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/timetable/PrintTimetable.java
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/timetable/PrintTimetable.java
@@ -1,5 +1,9 @@
 package fr.umlv.smoreau.beontime.client.actions.timetable;
 
+import java.awt.Graphics;
+import java.awt.JobAttributes;
+import java.awt.PageAttributes;
+import java.awt.PrintJob;
 import java.awt.event.ActionEvent;
 
 import fr.umlv.smoreau.beontime.client.actions.Action;
@@ -33,5 +37,23 @@ public class PrintTimetable extends Action {
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
      */
     public void actionPerformed(ActionEvent arg0) {
+		JobAttributes jobAttributes = new JobAttributes();
+		jobAttributes.setDestination(JobAttributes.DestinationType.PRINTER);
+		
+		PageAttributes pageAttributes = new PageAttributes();
+		pageAttributes.setOrientationRequested(PageAttributes.OrientationRequestedType.LANDSCAPE);
+		pageAttributes.setColor(PageAttributes.ColorType.COLOR);
+		pageAttributes.setOrigin(PageAttributes.OriginType.PRINTABLE);
+		pageAttributes.setMedia(PageAttributes.MediaType.A4);
+		
+		PrintJob demandeDImpression = mainFrame.getTable().getToolkit().getPrintJob(mainFrame.getMainFrame(), "Impression", jobAttributes, pageAttributes);
+		if (demandeDImpression != null) {
+			Graphics gImpr = demandeDImpression.getGraphics();
+
+			mainFrame.getTable().printAll(gImpr);
+
+			gImpr.dispose();
+			demandeDImpression.end();
+		}
     }
 }
