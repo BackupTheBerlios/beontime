18f6ffa40d884f3dae2eebf95bf107c50b135780
cstefkivoila <cstefkivoila>
2005-03-24 09:46:01 +0000

    nettoyage

--- beontimeSwing/src/fr/umlv/smoreau/beontime/client/graphics/windows/ManageSubjectsAdapter.java
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/client/graphics/windows/ManageSubjectsAdapter.java
@@ -17,10 +17,9 @@ import fr.umlv.smoreau.beontime.model.timetable.Timetable;
 
 /**
  * Manages the adapter on subjects has model
- * @author BeOnTime team
+ * @author BeOnTime
  */
-public class ManageSubjectsAdapter /*implements TreeModel*/ extends DefaultTreeModel {
-
+public class ManageSubjectsAdapter extends DefaultTreeModel {
 	private BoTModel model;
 	private final EventListenerList list;
 	private JTree tree;
@@ -30,43 +29,13 @@ public class ManageSubjectsAdapter /*implements TreeModel*/ extends DefaultTreeM
 		this.model = model;
 		this.list = new EventListenerList();
 		this.tree = t;
-		//System.out.println("subject adapteur");
-		
-		//TODO pour tester en local
-	/*	Timetable timetable = new Timetable();
-		Formation formation = new Formation();
-		formation.setHeading("DESS CRI");
-		formation.setIdFormation(new Long(1));
-		timetable.setFormation(formation);
-		
-		
-		
-		Subject subject = new Subject();
-		subject.setHeading("Matière1");
-		subject.setIdSubject(new Long(1));
-		Subject subject2 = new Subject();
-		subject2.setHeading("Matière2");
-		subject2.setIdSubject(new Long(2));
-		Subject subject3 = new Subject();
-		subject3.setHeading("Matière3");
-		subject3.setIdSubject(new Long(3));
-		ArrayList list = new ArrayList();
-		list.add(subject);
-		list.add(subject2);
-		list.add(subject3);
-		timetable.setSubjects(list);
-		
-		model.setTimetable(timetable);
-		//finTODO
-	 */
-
-	 //   model.setTimetable(e.getTimetable());
 
 	    //TODO améliorer
 	    tree.updateUI();
 	    tree.setVisible(true);
 		model.addBoTListener(new ManageSubjectListener());
 	}
+
 	/* (non-Javadoc)
 	 * @see javax.swing.tree.TreeModel#getRoot()
 	 */
@@ -121,7 +90,6 @@ public class ManageSubjectsAdapter /*implements TreeModel*/ extends DefaultTreeM
 	 * @see javax.swing.tree.TreeModel#getIndexOfChild(java.lang.Object, java.lang.Object)
 	 */
 	public int getIndexOfChild(Object parent, Object child) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
@@ -130,7 +98,6 @@ public class ManageSubjectsAdapter /*implements TreeModel*/ extends DefaultTreeM
 	 */
 	public void addTreeModelListener(TreeModelListener l) {
 		list.add(TreeModelListener.class, l);
-
 	}
 
 	/* (non-Javadoc)
@@ -138,25 +105,10 @@ public class ManageSubjectsAdapter /*implements TreeModel*/ extends DefaultTreeM
 	 */
 	public void removeTreeModelListener(TreeModelListener l) {
 		list.remove(TreeModelListener.class, l);
-
 	}
-	
 
 	
 	private class ManageSubjectListener extends DefaultBoTListener {
-		public void refreshAll(BoTEvent e) {
-		    model.setTimetable(e.getTimetable());
-
-		    //TODO améliorer
-		    tree.updateUI();
-		    tree.setVisible(true);
-		}
-	/*	
-		public void closeTimetable(BoTEvent e) {
-		    //TODO Ad: regarde si ça te plait. Moi ce qui m'embête c'est qu'au début c'est blanc et que quand on ferme l'emploi du temps, c'est gris ... :o(
-		    tree.setVisible(false);
-		}
-	*/	
 		public void addSubject(BoTEvent e) {
 		    tree.updateUI();
 		    tree.setVisible(true);
