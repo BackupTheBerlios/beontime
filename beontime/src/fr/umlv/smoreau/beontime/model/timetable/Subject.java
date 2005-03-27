0910fdcfc98ef6d4432e4ad477c00788fd3c34e9
cstefkivoila <cstefkivoila>
2005-03-27 00:28:39 +0000

    ajout du getter pour la couleur directement sur le serveur

--- beontime/src/fr/umlv/smoreau/beontime/model/timetable/Subject.java
+++ beontime/src/fr/umlv/smoreau/beontime/model/timetable/Subject.java
@@ -1,5 +1,8 @@
 package fr.umlv.smoreau.beontime.model.timetable;
 
+import java.awt.Color;
+import java.util.HashSet;
+
 import fr.umlv.smoreau.beontime.model.base.BaseSubject;
 
 /**
@@ -35,4 +38,47 @@ public class Subject extends BaseSubject {
 	}
 
 /*[CONSTRUCTOR MARKER END]*/
+	
+	private static final SubjectColor color = new SubjectColor();
+
+	public Color getColor() {
+	    return color.getColorAt(getIdSubject().intValue());
+	}
+
+
+	private static class SubjectColor {
+		private HashSet colors;
+		
+		public SubjectColor() {
+		    colors = new HashSet();
+			
+			colors.add(new Color(255, 255, 153));
+			colors.add(new Color(255, 255, 102));
+			colors.add(new Color(255, 204, 102));
+			colors.add(new Color(255, 153, 102));
+			colors.add(new Color(255, 204, 153));
+			colors.add(new Color(255, 204, 255));
+			colors.add(new Color(255, 153, 255));
+			colors.add(new Color(255, 153, 153));
+			colors.add(new Color(204, 255, 255));
+			colors.add(new Color(153, 204, 255));
+			colors.add(new Color(204, 204, 255));
+			colors.add(new Color(204, 153, 255));
+			colors.add(new Color(204, 102, 255));
+			colors.add(new Color(204, 255, 153));
+			colors.add(new Color(153, 255, 102));
+			colors.add(new Color(102, 255, 102));
+			colors.add(new Color(204, 204, 102));
+			colors.add(new Color(204, 153, 102));
+			colors.add(new Color(204, 204, 153));
+			colors.add(new Color(204, 204, 204));
+		}
+
+		public Color getColorAt(int index){
+			if (index > colors.size())
+			    return getColorAt(index - colors.size());
+			
+			return (Color) colors.toArray()[index];
+		}
+	}
 }
