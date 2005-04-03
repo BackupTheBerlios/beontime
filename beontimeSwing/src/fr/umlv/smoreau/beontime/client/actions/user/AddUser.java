bdcc9cf30f059c3d3395be8b837c00871a39b28c
cstefkivoila <cstefkivoila>
2005-04-03 11:11:33 +0000

    correction du bug avec le ' dans le password

--- beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/user/AddUser.java
+++ beontimeSwing/src/fr/umlv/smoreau/beontime/client/actions/user/AddUser.java
@@ -90,21 +90,24 @@ public class AddUser extends Action {
     private static char[] pwdChars = "abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
     
     private static final String generatePassword(int length) throws RemoteException, HibernateException {
-        char[] pwd = new char[length]; 
+        String password = null;
         try { 
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG"); 
-            
-            byte[] intbytes = new byte[4];       
-            
-            for (int i = 0; i < length; ++i) { 
-                random.nextBytes(intbytes); 
-                pwd[i] = pwdChars[Math.abs(getIntFromByte(intbytes) % pwdChars.length)]; 
-            } 
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            byte[] intbytes = new byte[4];
+            char[] pwd = new char[length];
+
+            do {
+	            for (int i = 0; i < length; ++i) {
+	                random.nextBytes(intbytes);
+	                pwd[i] = pwdChars[Math.abs(getIntFromByte(intbytes) % pwdChars.length)];
+	            }
+	            password = new String(pwd);
+            } while (password.indexOf('\'') != -1);
         } 
-        catch (Exception ex) { 
-            // Don't really worry, we won't be using this if we can't use securerandom anyway 
-        } 
-        return new String(pwd); 
+        catch (Exception ex) {
+            // Don't really worry, we won't be using this if we can't use securerandom anyway
+        }
+        return password;
     }
     
     private static int getIntFromByte(byte[] bytes) { 
